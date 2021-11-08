569da1762b210c4ed4800da748a68daafbd400f1
Myrtleyertle <89166514+Myrtleyertle@users.noreply.github.com>
2021-11-08 01:50:05 -0600

    update

--- app.js
+++ app.js
@@ -20,24 +20,22 @@ const socialCurrent = document.querySelector('[data-social-current-time]')
 const socialPrevious = document.querySelector('[data-social-previous-time]')
 const selfCareCurrent = document.querySelector('[data-self-care-current-time]')
 const selfCarePrevious = document.querySelector('[data-self-care-previous-time]')
+const daily = document.querySelector('[data-daily-tracker]');
+const weekly = document.querySelector('[data-weekly-tracker]');
+const monthly = document.querySelector('[data-monthly-tracker]');
+
 
 function weeklyFetch() {
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
-
+            console.log(data)
             const workArr = data.slice(0, 1, 3);
             const playArr = data.slice(1, 2, 3);
             const exerciseArr = data.slice(3, 4, 3);
             const studyArr = data.slice(2, 3, 3);
             const socialArr = data.slice(4, 5, 3);
             const selfCareArr = data.slice(5, 6, 3);
-            console.log(selfCareArr);
-            console.log(socialArr);
-            console.log(studyArr);
-            console.log(exerciseArr);
-            console.log(playArr);
-            console.log(workArr);
             for (data of workArr) {
                 const workCurrentTime = data.timeframes.weekly.current;
                 const workPreviousTime = data.timeframes.weekly.previous;
@@ -90,12 +88,6 @@ function monthlyFetch() {
             const studyArr = data.slice(2, 3, 3);
             const socialArr = data.slice(4, 5, 3);
             const selfCareArr = data.slice(5, 6, 3);
-            console.log(selfCareArr);
-            console.log(socialArr);
-            console.log(studyArr);
-            console.log(exerciseArr);
-            console.log(playArr);
-            console.log(workArr);
             for (data of workArr) {
                 const workCurrentTime = data.timeframes.monthly.current;
                 const workPreviousTime = data.timeframes.monthly.previous;
@@ -135,5 +127,59 @@ function monthlyFetch() {
         })
 }
 
+function dailyFetch() {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const workArr = data.slice(0, 1, 3);
+            const playArr = data.slice(1, 2, 3);
+            const exerciseArr = data.slice(3, 4, 3);
+            const studyArr = data.slice(2, 3, 3);
+            const socialArr = data.slice(4, 5, 3);
+            const selfCareArr = data.slice(5, 6, 3);
+            for (data of workArr) {
+                const workCurrentTime = data.timeframes.daily.current;
+                const workPreviousTime = data.timeframes.daily.previous;
+                workCurrent.innerHTML = `${workCurrentTime}Hrs`;
+                workPrevious.innerHTML = `${workPreviousTime}Hrs`;
+            }
+            for (data of playArr) {
+                const playCurrentTime = data.timeframes.daily.current;
+                const playPreviousTime = data.timeframes.daily.previous;
+                playCurrent.innerHTML = `${playCurrentTime}Hrs`;
+                playPrevious.innerHTML = `${playPreviousTime}Hrs`;
+            }
+            for (data of exerciseArr) {
+                const exerciseCurrentTime = data.timeframes.daily.current;
+                const exercisePreviousTime = data.timeframes.daily.previous;
+                exerciseCurrent.innerHTML = `${exerciseCurrentTime}Hrs`;
+                exercisePrevious.innerHTML = `${exercisePreviousTime}Hrs`;
+            }
+            for (data of studyArr) {
+                const studyCurrentTime = data.timeframes.daily.current;
+                const studyPreviousTime = data.timeframes.daily.previous;
+                studyCurrent.innerHTML = `${studyCurrentTime}Hrs`;
+                studyPrevious.innerHTML = `${studyPreviousTime}Hrs`;
+            }
+            for (data of socialArr) {
+                const socialCurrentTime = data.timeframes.daily.current;
+                const socialPreviousTime = data.timeframes.daily.previous;
+                socialCurrent.innerHTML = `${socialCurrentTime}Hrs`;
+                socialPrevious.innerHTML = `${socialPreviousTime}Hrs`;
+            }
+            for (data of selfCareArr) {
+                const selfCareCurrentTime = data.timeframes.daily.current;
+                const selfCarePreviousTime = data.timeframes.daily.previous;
+                selfCareCurrent.innerHTML = `${selfCareCurrentTime}Hrs`;
+                selfCarePrevious.innerHTML = `${selfCarePreviousTime}Hrs`;
+            }
+        })
+}
+
+
+
+daily.addEventListener('click', dailyFetch);
+weekly.addEventListener('click', weeklyFetch);
+monthly.addEventListener('click', monthlyFetch);
 
 weeklyFetch();
