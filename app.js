60e3e1e2aeb601756e4bce2a753eb62973de1f8f
Myrtleyertle <89166514+Myrtleyertle@users.noreply.github.com>
2021-11-08 00:58:54 -0600

    update

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ kebab.addEventListener('click', function() {
     cross.classList.toggle('active');
     dropdown.classList.toggle('active');
 })
+const workCurrent = document.querySelector('[data-work-time]')
+
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+
+        const workArr = data.slice(0, 1, 3);
+        console.log(workArr);
+        for (data of workArr) {
+            const workTime = data.timeframes.weekly.current;
+            workCurrent.innerHTML = `${workTime}Hrs`;
+        }
+
+    })
+
+.catch(error => console.log(error));
